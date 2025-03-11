7ff9c96d732461e0edcbe6e0ffa4340a908357aa
rajat <rajat290@users.noreply.github.com>
2025-03-11 22:27:19 +0530

    completed 3/20 question of important Interview react question

---
+++ 01TOP 20 Questions of React From Intellipath/PracticalQuestions/Task1Create a simple React app.js
@@ -0,0 +1,5 @@
+// ✅ 1. Problem/Task for You:
+// 👉 Task: Create a simple React app that displays a text message like:
+
+// "Hello, My Name is [Your Name] and I am preparing for my React Interview."
+// "React is amazing!
---
+++ 01TOP 20 Questions of React From Intellipath/PracticalQuestions/firstApp/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>"Hello, My Name is Rajat Singh <br />
+      and I am preparing for my React Interview."
+    </h1>
+    <p>Raect Is Amazing!!</p>
+    </>
+  )
+}
+
+export default App
