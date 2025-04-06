c311de22eb5d5a8172b219b829072f83bd37eb35
rajat <rajat290@users.noreply.github.com>
2025-04-07 02:37:58 +0530

    completed the Search bar practice project

--- 07.Machine Coding Interview Frontend - React/02. AutoComplete SearchBar/Search bar/src/App.jsx
+++ 07.Machine Coding Interview Frontend - React/02. AutoComplete SearchBar/Search bar/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([])
+
+
+const fetchData = async () => {
+
+  const data = await fetch("https://dummyjson.com/recipes/search?q=" + input);
+  const json = await data.json();
+setResults(json?.recipes);
+};
+
+useEffect(() => {
+  fetchData();
+} , [input])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <h1> AutoComplete Search Bar</h1>
+        <input type="text" 
+        placeholder="search" 
+        className="search-bar"
+        value={input} 
+        onChange={(e) => setInput(e.target.value)}/>
+        <div className="results-container">
+        {results.map((r)=> <span className="result"key={r.id}>{r.name}</span>)}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
